b729e0a0634ed29fc491f9ae86c433098ae93630
tahmid <tahmid.hassan@gmail.com>
2015-06-12 16:23:08 +0600

    Initial Commit

---
+++ app/src/main/java/com/bracu/thesis/medqueue/app/AppConfig.java
@@ -0,0 +1,9 @@
+package com.bracu.thesis.medqueue.app;
+
+/**
+ * Created by Tahmid on 21-May-15.
+ */
+public class AppConfig {
+
+    public static String URL_LOGIN = "";
+}
---
+++ app/src/main/java/com/bracu/thesis/medqueue/helper/SessionManager.java
@@ -0,0 +1,7 @@
+package com.bracu.thesis.medqueue.helper;
+
+/**
+ * Created by Tahmid on 21-May-15.
+ */
+public class SessionManager {
+}
