d67e257b9d36017580f2ba444ba3833cbc4f5cf1
tahmid <tahmid.hassan@gmail.com>
2015-08-03 11:26:44 +0600

    Initial Commit

--- app/src/main/java/com/bracu/thesis/medqueue/app/AppController.java
+++ app/src/main/java/com/bracu/thesis/medqueue/app/AppController.java
@@ -39,5 +39,14 @@ public class AppController extends Application{
         req.setTag(TextUtils.isEmpty(tag) ? TAG : tag);
         getRequestQueue().add(req);
     }
+    public <T> void addToRequestQueue(Request<T> req){
+        req.setTag(TAG);
+        getRequestQueue().add(req);
+    }
+    public void cancelPendingRequest(Object tag){
+        if(mRequestQueue != null){
+            mRequestQueue.cancelAll(tag);
+        }
+    }
 
 }
